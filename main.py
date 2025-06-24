72a6fb8383139efae6cdce5f31c49b0080e386be
Kristina Cas <m.kristinacas@gmail.com>
2025-06-24 21:42:08 +0800

    Add: function for running the blank window of game

--- main.py
+++ main.py
@@ -9,3 +9,19 @@ win_height = 720
 win_width = 551
 window = pygame.display.set_mode((win_width, win_height))
 
+def quit_game():
+    pass
+
+def main():
+    run = True
+    while run:
+        quit_game()
+        
+        window.fill (0, 0, 0)
+        
+        timer.tick(60)
+        pygame.display.update()
+        
+main()
+        
+        

fe8c8ca0ab950df97c824cb24e4c2876a61c0831
Kristina Cas <m.kristinacas@gmail.com>
2025-06-24 21:43:12 +0800

    Add: function for allowing user to quit game

--- main.py
+++ main.py
@@ -10,7 +10,10 @@ win_width = 551
 window = pygame.display.set_mode((win_width, win_height))
 
 def quit_game():
-    pass
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
 
 def main():
     run = True

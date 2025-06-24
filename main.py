fa30f420ee2f4b5c571e8c3e9934843c907c2792
Kristina Cas <m.kristinacas@gmail.com>
2025-06-24 21:58:33 +0800

    Add: imported images into the workspace

--- main.py
+++ main.py
@@ -10,6 +10,17 @@ win_height = 720
 win_width = 551
 window = pygame.display.set_mode((win_width, win_height))
 
+# load images
+bird_imgs = [pygame.image.load("assets/bird_down.png"),
+             pygame.image.load("assets/bird_mid.png"),
+             pygame.image.load("assets/bird_up.png"),]
+bg_img = pygame.image.load("assets/background.png")
+ground_img = pygame.image.load("assets/ground.png")
+top_pipe_img = pygame.image.load("assets/pipe_top.png")
+bot_pipe_img = pygame.image.load("assets/pipe_bottom.png")
+start_img = pygame.image.load("assets/start.png")
+game_over_img = pygame.image.load("assets/game_over.png")
+
 # exit the game
 def quit_game():
     for event in pygame.event.get():

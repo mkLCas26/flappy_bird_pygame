41a2a044895d08557c1bc7f7685c32d44b86339a
Kristina Cas <m.kristinacas@gmail.com>
2025-06-24 21:40:00 +0800

    Add: initialize pygame, set timer,  and initialize game window

---
+++ main.py
@@ -0,0 +1,11 @@
+# import libraries and other necessities
+import pygame
+from sys import exit
+
+pygame.init()
+timer = pygame.time.Clock()
+
+win_height = 720
+win_width = 551
+window = pygame.display.set_mode((win_width, win_height))
+

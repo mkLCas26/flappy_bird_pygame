923ae6de6fce195c5f1b687b930b546562965fdf
Kristina Cas <m.kristinacas@gmail.com>
2025-06-25 20:38:28 +0800

    Add: init at qui_game method

--- game.py
+++ game.py
@@ -8,5 +8,17 @@ from pipe import Pipe
 from ground import Ground
 
 class Game:
-    def __init__(self):\
-        pass
+    def __init__(self):
+        score = 0
+        game_stopped = True
+        self.window = window 
+        timer = pygame.time.Clock()
+        font = pygame.font.SysFont("Segoe", 26)
+        
+    def quit_game(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+        
+        

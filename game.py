ae1972a82c86b330108f3fbdbfe5986dc9a5b5e7
Kristina Cas <m.kristinacas@gmail.com>
2025-06-25 20:05:01 +0800

    Add: imports and new class for organization of code

---
+++ game.py
@@ -0,0 +1,12 @@
+import pygame
+import random
+from sys import exit
+
+from utils import *
+from bird import Bird
+from pipe import Pipe
+from ground import Ground
+
+class Game:
+    def __init__(self):\
+        pass
